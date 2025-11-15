4f4dcf70d8d7dfbef8fab7f6ddc12bc73979323d
vowdemon <holy.god@qq.com>
2025-11-15 17:33:44 +0800

    refactor(setup): consolidate hooks exports

--- packages/jolt_flutter/lib/setup.dart
+++ packages/jolt_flutter/lib/setup.dart
@@ -1,9 +1,4 @@
 library;
 
-export 'src/setup/hooks/animation.dart';
-export 'src/setup/hooks/focus.dart';
-export 'src/setup/hooks/lifecycle.dart';
-export 'src/setup/hooks/scroll.dart';
-export 'src/setup/hooks/text.dart';
-export 'src/setup/hooks/jolt.dart';
+export 'src/setup/hooks/hooks.dart';
 export 'src/setup/widget.dart';
---
+++ packages/jolt_flutter/lib/src/setup/hooks/hooks.dart
@@ -0,0 +1,6 @@
+export 'jolt.dart';
+export 'lifecycle.dart';
+export 'scroll.dart';
+export 'text.dart';
+export 'animation.dart';
+export 'focus.dart';

89493ee7de930392322226f5035a10dcdd137fb1
vowdemon <holy.god@qq.com>
2026-04-07 15:53:34 +0800

    chore(tests): switch from 'test' to 'flutter_test' package for creation stack formatter tests

--- packages/jolt_devtools_extension/test/utils/creation_stack_formatter_test.dart
+++ packages/jolt_devtools_extension/test/utils/creation_stack_formatter_test.dart
@@ -1,5 +1,5 @@
 import 'package:jolt_devtools_extension/src/utils/creation_stack_formatter.dart';
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('formatCreationStackForDisplay', () {

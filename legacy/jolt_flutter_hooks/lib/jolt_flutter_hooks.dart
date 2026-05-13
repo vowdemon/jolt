bd6711f8412cd1561a08b3574f55dc91872e6ac4
vowdemon <holy.god@qq.com>
2026-05-13 10:24:28 +0800

    chore: remove legacy from main

--- legacy/jolt_flutter_hooks/lib/jolt_flutter_hooks.dart
+++
@@ -1,9 +0,0 @@
-/// Declarative hooks for Flutter widgets.
-///
-/// This library provides convenient hooks for managing common Flutter resources
-/// like controllers, focus nodes, and lifecycle states with automatic cleanup.
-/// Designed to work with SetupWidget from jolt_flutter.
-@Deprecated('jolt_flutter_hooks is deprecated. Use jolt_setup instead.')
-library;
-
-export 'package:jolt_setup/hooks.dart';
